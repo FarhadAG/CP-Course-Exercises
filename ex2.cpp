1cfedd272136ba71ae91d8a4ed1889d024ed2ec1
Farhad Aghasi <122684104+FarhadAG@users.noreply.github.com>
2024-02-13 01:55:56 +0330

    Add files via upload
    The secend exercise of the lesson.

---
+++ ex2.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << char(70) << char(97) << char(114) << char(104) << char(97) << char(100) << ' ' << char(65) << char(103) << char(104) << char(97) << char(115) << char(105) << std::endl;
+    return 0;
+}
